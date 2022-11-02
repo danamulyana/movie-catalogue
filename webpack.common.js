2d98143caa85edf210b09797664ade23fa133e02
Dana Mulyana <danabontot@gmail.com>
2022-11-02 15:29:06 +0700

    refactor workbox

--- webpack.common.js
+++ webpack.common.js
@@ -1,13 +1,11 @@
 /* eslint-disable indent */
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
-const WorkboxWebpackPlugin = require('workbox-webpack-plugin');
 const path = require('path');
 
 module.exports = {
     entry: {
-      app: path.resolve(__dirname, 'src/scripts/index.js'),
-      // sw: path.resolve(__dirname, 'src/scripts/sw.js'),
+      app: path.resolve(__dirname, 'src/scripts/index.js')
     },
     output: {
       filename: '[name].bundle.js',
@@ -41,10 +39,6 @@ module.exports = {
             to: path.resolve(__dirname, 'dist/'),
           },
         ],
-      }),
-      new WorkboxWebpackPlugin.InjectManifest({
-        swSrc: path.resolve(__dirname, 'src/scripts/ws.js'),
-        swDest: './sw.bundle.js',
       }),
     ],
 };

5bb6a85a82134ed72a252fb63258d648db2c7825
Dana Mulyana <danabontot@gmail.com>
2022-11-02 13:47:48 +0700

    notification init

---
+++ src/scripts/utils/websocket-initiator.js
@@ -0,0 +1,14 @@
+const webSocketInitiator = {
+  init(url) {
+    const webSocket = new WebSocket(url);
+    webSocket.onmessage = this._onMessageHandler;
+  },
+
+  _onMessageHandler(message) {
+    console.log(message);
+    console.log(message.data);
+    console.log(JSON.parse(message.data));
+  },
+};
+
+export default webSocketInitiator;
